6eda61a2defa0508a07e563c51853ad82aca4de7
Rumy <personalrumy@gmail.com>
2026-03-25 14:28:49 +0700

    feat: add business process content

--- zudoku.config.tsx
+++ zudoku.config.tsx
@@ -38,6 +38,24 @@ const config: ZudokuConfig = {
                   "file": "home/tools/uv",
                   "label": "uv",
                   "icon": "bolt"
+                },
+                {
+                  "type": "doc",
+                  "file": "home/tools/ruff",
+                  "label": "ruff",
+                  "icon": "bolt"
+                },
+                {
+                  "type": "doc",
+                  "file": "home/tools/github",
+                  "label": "GitHub",
+                  "icon": "bolt"
+                },
+                {
+                  "type": "doc",
+                  "file": "home/tools/roo-code",
+                  "label": "Roo Code",
+                  "icon": "bolt"
                 }
               ]
             },
@@ -64,8 +82,8 @@ const config: ZudokuConfig = {
           "collapsed": true,
           "items": [
             {
-              "type": "doc",
-              "file": "research/research-template",
+              "type": "link",
+              "to": "https://docs.google.com/document/d/1sLtLpA9QeOQZYJacwBMw-u1pnVL8ODSr5XTYZwGxpwc/edit?usp=sharing",
               "label": "Report Template",
               "icon": "file-text"
             }
@@ -78,6 +96,26 @@ const config: ZudokuConfig = {
     label: "Prototyping",
     items: [
       "/prototyping",
+      {
+        "type": "category",
+        "label": "PoC",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "blocks",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Paid PoC",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "circle-dollar-sign",
+        "items": [
+
+        ]
+      }
     ],
   },
   {
@@ -85,13 +123,66 @@ const config: ZudokuConfig = {
     label: "Development",
     items: [
       "/development",
-    ],
-  },
-  {
-    type: "category",
-    label: "Testing",
-    items: [
-      "/testing",
+      {
+        "type": "category",
+        "label": "Development Standard",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "code",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Computer Vision",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "eye",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Machine Learning",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "brain",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Large Language Models",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "bot",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Business Automation",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "settings",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Testing",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "flask-conical",
+        "items": [
+
+        ]
+      },
     ],
   },
   {
@@ -99,6 +190,53 @@ const config: ZudokuConfig = {
     label: "Deployment",
     items: [
       "/deployment",
+      {
+        "type": "category",
+        "label": "Edge Deployment",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "cpu",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Cloud Deployment",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "cloud",
+        "items": [
+
+        ]
+      },
+    ],
+  },
+    {
+    type: "category",
+    label: "Monitoring & Iteration",
+    items: [
+      "/monitoring-iteration",
+      {
+        "type": "category",
+        "label": "Documentation",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "book",
+        "items": [
+
+        ]
+      },
+      {
+        "type": "category",
+        "label": "Bug Fixing",
+        "collapsible": true,
+        "collapsed": true,
+        "icon": "bug",
+        "items": [
+
+        ]
+      },
     ],
   },
 ],
